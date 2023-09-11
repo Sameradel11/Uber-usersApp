8af9609b18af20f62722a04327db65628865ee2d
samer <sameradel789@gmail.com>
2023-09-11 05:41:58 +0300

    fix googlmaps freeze

--- lib/Features/home/homeview.dart
+++ lib/Features/home/homeview.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:user_app/Features/Autentication/presentation/view_model/cubits/authcubit/auth_cubit.dart';
-import 'package:user_app/core/app_routes.dart';
 import 'package:user_app/core/const.dart';
 
 class HomeView extends StatefulWidget {
@@ -16,30 +12,21 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   GoogleMapController? mapcontroller;
-  final Completer<GoogleMapController> _controller =
-      Completer();
+  final mycontroller = Completer();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        floatingActionButton: IconButton(
-          icon: const Icon(Icons.logout, size: 40),
-          onPressed: () {
-            BlocProvider.of<AuthCubit>(context).signout();
-            GoRouter.of(context).pushReplacement(AppRoutes.splashview);
-          },
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-        body: Center(
-          child: GoogleMap(
-              myLocationEnabled: true,
-              mapType: MapType.normal,
-              initialCameraPosition: kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-                mapcontroller = controller;
-              }),
-        ),
+    return Scaffold(
+    body: Container(
+    height: 700,
+    child: GoogleMap(
+      myLocationEnabled: true,
+      mapType: MapType.normal,
+      initialCameraPosition: kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        mycontroller.complete(controller);
+        mapcontroller = controller;
+      },
+    ),
       ),
     );
   }
